7ed583fb155064ac60be078192d311b9de14068a
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2022-06-27 15:54:53 +0100

    Added about and skills sections

--- src/assets/scripts/main.ts
+++ src/assets/scripts/main.ts
@@ -1,1 +1,22 @@
+const tabs = document.querySelectorAll<HTMLDivElement>('.skills-tab');
+const tabsContent = document.querySelectorAll<HTMLDivElement>('.skills-group');
 
+tabs.forEach((tab) => {
+	tab.addEventListener('click', () => {
+		const target = document.querySelector<HTMLDivElement>(
+			tab.dataset.target ?? ''
+		);
+
+		tabsContent.forEach((tabContent) => {
+			tabContent.classList.remove('skills-active');
+		});
+
+		target?.classList.add('skills-active');
+
+		tabs.forEach((tab) => {
+			tab.classList.remove('skills-active');
+		});
+
+		tab?.classList.add('skills-active');
+	});
+});
