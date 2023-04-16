28c47dd0073158eb2da8707e70d8b5aff204b4ba
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2023-04-16 07:30:13 +0100

    Added vite-plugin-pwa for better assets caching and offline functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 import { dirname, resolve } from 'path';
 import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import solidPlugin from 'vite-plugin-solid';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-	plugins: [solidPlugin()],
+	plugins: [
+		solidPlugin(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+			},
+		}),
+	],
 	resolve: {
 		alias: [{ find: '~', replacement: resolve(__dirname, 'src') }],
 	},
-	server: {
-		port: 3000,
-	},
-	build: {
-		target: 'esnext',
-	},
+	server: { port: 3000 },
+	build: { target: 'esnext' },
 });
