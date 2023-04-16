b1033999d11d23ecfa46432fe3fec00cdcc92eec
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2023-04-16 23:01:57 +0100

    Added pdf format to the vitePWA list of cached files

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 		VitePWA({
 			registerType: 'autoUpdate',
 			workbox: {
-				globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+				globPatterns: ['**/*.{js,css,html,ico,png,svg,pdf}'],
 			},
 		}),
 	],
