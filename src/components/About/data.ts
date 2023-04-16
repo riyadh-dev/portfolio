fa8124268b5a867acf04c1d3be6753a835daa476
Baatchia Riyadh <Baatchia_Riyadh@protonmail.com>
2023-04-16 07:27:54 +0100

    Minor adjustments here and there

--- src/components/About/data.ts
+++ src/components/About/data.ts
@@ -1,7 +1,7 @@
 const ABOUT = {
 	introduction:
 		'Full Stack Web developer with a master’s degree in Computer Science. I have several years of experience in Web and Desktop development, focusing on frontend development (ReactJS) as well as back-End development (NodeJS). I am self-motivated person, fast learner and open to new challenges for the development of my skills.',
-	projectsDone: '+ 9',
+	projectsDone: '+ 10',
 	gitHubCommits: '+ 600',
 };
 
