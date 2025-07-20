cdfe2253af6eef9dfd14dce781ebeeb7c91af31e
riyadh-dev <riyadh-dev@proton.me>
2025-07-21 00:13:04 +0100

    moved inline svgs to separate files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,7 @@ import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		icon({
-			iconDir: './src/assets/images/logos',
-		}),
-	],
+	integrations: [icon()],
 	server: {
 		port: 3000,
 	},
